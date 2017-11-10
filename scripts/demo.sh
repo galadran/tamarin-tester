c3022af436fbd076d39f84c495577b38de3a9607
Dennis Jackson <dennis.jackson@cs.ox.ac.uk>
2017-11-10 23:19:46 +0000

    Initial commit to v2.0

--- scripts/demo.sh
+++
@@ -1,5 +0,0 @@
-rm data/demo_protocols/demo.res
-asciinema -y -c  "./dist/tamarin-tester /home/dennis/.local/bin/tamarin-prover -p data/demo_protocols/ --input data/demo_protocols/demo_good.res"
-asciinema -y -c  "./dist/tamarin-tester /home/dennis/.local/bin/tamarin-prover -p data/demo_protocols/ --input data/demo_protocols/demo_bad.res -mp 2 --overtime"
-asciinema -y -c  "./dist/tamarin-tester /home/dennis/.local/bin/tamarin-prover -p data/demo_protocols/ --benchmark -mp 15 -mc 10 --output data/demo_protocols/demo.res"
-rm data/demo_protocols/demo.res
